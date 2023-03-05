952cd9595df7eef58d96108d8b163ed977a7c345
evanpurl <evan@purlcomputers.com>
2023-03-05 13:42:17 -0700

    Fixed issue with receiving bot pfp

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import requests
 from database.database import findbots
 from database.testdbconnection import connect
 
-#hostName = "0.0.0.0"
-hostName = "localhost"
+hostName = "0.0.0.0"
 serverPort = 25566
 
 
@@ -16,6 +15,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>Purls' Bot status page</title></head>", "utf-8"))
+        self.wfile.write(bytes("<style> h1 {text-align: center;} h2 {text-align: center;} p {text-align: center;} </style>", "utf-8"))
         self.wfile.write(bytes("<h1>Bot statuses:</h1>", "utf-8"))
         if connect():
             self.wfile.write(bytes("<h2>Database Connection Verified</h2>", "utf-8"))
@@ -24,11 +24,12 @@ class MyServer(BaseHTTPRequestHandler):
             for a in findbots():
                 response = requests.get(a[1], timeout=10)
                 if response.status_code == 200:
-                    self.wfile.write(bytes(f"<p>{a[0]}: Online</p>", "utf-8"))
+                    data = response.json()
+                    self.wfile.write(bytes(f"<p style='display: block;margin-left: auto;margin-right: auto;'> <img src='{list(data.values())[0]}' alt='{a[0]} pfp' width='30' height='30'>{a[0]}: Online</p>", "utf-8"))
         except requests.exceptions.Timeout:
-            self.wfile.write(bytes(f"<p>{a[0]}: Offline</p>", "utf-8"))
+            self.wfile.write(bytes(f"<p style='display: block;margin-left: auto;margin-right: auto;'>{a[0]}: Offline</p>", "utf-8"))
         except requests.exceptions.ConnectionError:
-            self.wfile.write(bytes(f"<p>{a[0]}: Offline</p>", "utf-8"))
+            self.wfile.write(bytes(f"<p style='display: block;margin-left: auto;margin-right: auto;'>{a[0]}: Offline</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 
